2b1391a0d20fdac9d7ec1ea3b0f8da6bac314958
Gergely Orosz <gergely.orosz@hotmail.com>
2011-05-31 22:28:36 +0200

    adding cache provider

---
+++ Research/Research.WebApplication/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Entites = Research.Services.Northwind.NorthwindDataSetEntities;
+using Research.Services.Northwind.NorthwindDataSetExtensions;
+
+namespace Research.WebApplication
+{
+    public partial class Default : Impl.ApplicationPage
+    {
+        public string GetFrom = string.Empty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            base.Page_Load(sender, e);
+            var data = NWCacheProvider.Get<IEnumerable<Entites.Product>>("NWProducts");
+            if (data == null)
+            {
+                GetFrom = "Database";
+                data = NWCacheProvider.Add<IEnumerable<Entites.Product>>("NWProducts", GetAll, 60, false);
+            }
+            else
+            {
+                GetFrom = "Cache";
+            }
+            NWDataSet.Product.Merge(data.AsDataTable());
+        }
+
+        IEnumerable<Entites.Product> GetAll()
+        {
+            return NWServiceManager.ProductRepository.GetAll().AsEntities();
+        }
+
+        public IEnumerable<Entites.Product> NWProducts
+        {
+            get
+            {
+                return NWDataSet.Product.AsEntities();
+            }
+        }
+        
+    }
+}
---
+++ Research/Research.WebApplication/Details.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Entites = Research.Services.Northwind.NorthwindDataSetEntities;
+using Research.Services.Northwind.NorthwindDataSetExtensions;
+
+namespace Research.WebApplication
+{
+    public partial class Details : Impl.ApplicationPage
+    {
+        public string GetFrom = string.Empty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            base.Page_Load(sender, e);
+            var data = NWCacheProvider.Get<IEnumerable<Entites.Product>>("NWProducts");
+            if (data == null)
+            {
+                GetFrom = "Database";
+                data = NWCacheProvider.Add<IEnumerable<Entites.Product>>("NWProducts", GetAll, 60, false);
+            }
+            else
+            {
+                GetFrom = "Cache";
+            }
+            NWDataSet.Product.Merge(data.AsDataTable());
+        }
+
+        IEnumerable<Entites.Product> GetAll()
+        {
+            return NWServiceManager.ProductRepository.GetAll().AsEntities();
+        }
+
+        public Entites.Product CurrentProduct
+        {
+            get
+            {
+                int id = -1;
+                if (!String.IsNullOrWhiteSpace(Request.QueryString["id"]))
+                    if (!int.TryParse(Request.QueryString["id"].Trim(), out id))
+                        id = -1;
+                if (id < 1)
+                    return null;
+                return NWDataSet.Product.SingleOrDefault(model => model.ProductID == id).AsEntity();
+            }
+        }
+    }
+}
