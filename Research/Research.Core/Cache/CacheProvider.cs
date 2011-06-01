1916810f9cd53cb931de270d8b4b224bf39036b9
Gergely Orosz <gergely.orosz@hotmail.com>
2011-06-01 07:16:07 +0200

    modifing cache in webapplication

---
+++ Research/Research.Core/Cache/CacheProvider.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Research.Core.Cache
+{
+    public class CacheProvider
+    {
+        public delegate T OnCacheItemAdding<T>();
+
+        private System.Web.Caching.Cache m_Cache;
+
+        public CacheProvider() : this(new System.Web.Caching.Cache()) { }
+
+        public CacheProvider(System.Web.Caching.Cache cache)
+        {
+            m_Cache = cache;
+        }
+
+        public T Get<T>(string key)
+        {
+            if (m_Cache == null)
+                return default(T);
+            if (m_Cache[key] == null)
+                return default(T);
+            return (T)m_Cache[key];
+        }
+
+        public T Add<T>(string uniqueKey, OnCacheItemAdding<T> action, int? seconds, bool isNeverExpire)
+        {
+            if(m_Cache != null)
+            {
+                if (m_Cache[uniqueKey] == null)
+                {
+                    T item = action.Invoke();
+                    if (item != null)
+                        m_Cache.Add(uniqueKey, item, null, DateTime.Now.AddSeconds(seconds.Value),
+                                        System.Web.Caching.Cache.NoSlidingExpiration,
+                                            ((isNeverExpire) ? System.Web.Caching.CacheItemPriority.NotRemovable : System.Web.Caching.CacheItemPriority.High), OnCacheItemRemovedCallback);
+                }
+            }
+            return Get<T>(uniqueKey);
+        }
+
+        public void OnCacheItemRemovedCallback(string key, object sender, System.Web.Caching.CacheItemRemovedReason callback)
+        {
+            IDisposable disposable = sender as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+            disposable = null;
+        }
+
+        public void Remove(string key)
+        {
+            if (m_Cache == null)
+                return;
+            
+            if (m_Cache[key] != null)
+            {
+                IDisposable disposable = m_Cache[key] as IDisposable;
+                if (disposable != null)
+                    disposable.Dispose();
+                disposable = null;
+                m_Cache[key] = null;
+                m_Cache.Remove(key);
+            }
+        }
+
+        public void Clean()
+        {
+            if (m_Cache == null)
+                return;
+            string[] keys = { };
+            IDictionaryEnumerator enu = m_Cache.GetEnumerator();
+            while (enu.MoveNext())
+                keys.Union(new[] { enu.Key });
+            foreach (string k in keys)
+            {
+                if (m_Cache[k] != null)
+                {
+                    IDisposable disposable = m_Cache[k] as IDisposable;
+                    if (disposable != null)
+                        disposable.Dispose();
+                    disposable = null;
+                    m_Cache[k] = null;
+                    m_Cache.Remove(k);
+                }
+            }
+        }
+    }
+}
---
+++ Research/Research.WebApplication/Impl/ApplicationPage.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Entites = Research.Services.Northwind.NorthwindDataSetEntities;
+using Research.Services.Northwind.NorthwindDataSetExtensions;
+
+namespace Research.WebApplication.Impl
+{
+    public class ApplicationPage : System.Web.UI.Page
+    {
+        private Research.DataAccess.Northwind.NorthwindDataSet m_DataSet = null;
+        private Research.DataAccess.Northwind.NorthwindDataSetTableAdapters.TableAdapterManager m_TableAdapterManager = null;
+        private Research.Services.Northwind.NorthwindDataSetServices.IServiceManager m_ServiceManager = null;
+        private Research.Core.Cache.CacheProvider m_CacheProvider = null;
+        Dictionary<string, bool> m_IsCached = null;
+
+        public ApplicationPage()
+        {
+            m_DataSet = new DataAccess.Northwind.NorthwindDataSet();
+            m_TableAdapterManager = new DataAccess.Northwind.NorthwindDataSetTableAdapters.TableAdapterManager();
+            m_ServiceManager = new Research.Services.Northwind.NorthwindDataSetServices.ServiceManager(m_TableAdapterManager);
+            m_IsCached = new Dictionary<string, bool>();
+            PreInit += new EventHandler(ApplicationPage_PreInit);
+        }
+
+        void ApplicationPage_PreInit(object sender, EventArgs e)
+        {
+            m_CacheProvider = new Core.Cache.CacheProvider(Cache);
+            LoadVars();
+        }
+
+        private void LoadVars()
+        {
+            var products = NWCacheProvider.Get<IEnumerable<Entites.Product>>("NWProducts");
+            m_IsCached.Add("NWProducts", !(products == null));
+            if (products == null)
+                products = NWCacheProvider.Add<IEnumerable<Entites.Product>>("NWProducts", Products_GetAll, 60, false);
+            NWDataSet.Product.Merge(products.AsDataTable());
+
+            var categories = NWCacheProvider.Get<IEnumerable<Entites.Category>>("NWCategories");
+            m_IsCached.Add("NWCategories", !(products == null));
+            if (categories == null)
+                categories = NWCacheProvider.Add<IEnumerable<Entites.Category>>("NWCategories", Categories_GetAll, 60, false);
+            NWDataSet.Category.Merge(categories.AsDataTable());
+
+            var suppliers = NWCacheProvider.Get<IEnumerable<Entites.Supplier>>("NWSuppliers");
+            m_IsCached.Add("NWSuppliers", !(products == null));
+            if (suppliers == null)
+                suppliers = NWCacheProvider.Add<IEnumerable<Entites.Supplier>>("NWSuppliers", Suppliers_GetAll, 60, false);
+            NWDataSet.Supplier.Merge(suppliers.AsDataTable());
+        }
+
+        IEnumerable<Entites.Product> Products_GetAll()
+        {
+            return NWServiceManager.ProductRepository.GetAll().AsEntities();
+        }
+
+        IEnumerable<Entites.Category> Categories_GetAll()
+        {
+            return NWServiceManager.CategoryRepository.GetAll().AsEntities();
+        }
+
+        IEnumerable<Entites.Supplier> Suppliers_GetAll()
+        {
+            return NWServiceManager.SupplierRepository.GetAll().AsEntities();
+        }
+
+        public override void Dispose()
+        {
+            if (m_TableAdapterManager != null)
+                m_TableAdapterManager.Dispose();
+            if (m_DataSet != null)
+                m_DataSet.Dispose();
+            m_ServiceManager = null;
+            m_TableAdapterManager = null;
+            m_DataSet = null;
+            base.Dispose();
+        }
+
+        protected Research.DataAccess.Northwind.NorthwindDataSet NWDataSet { get { return m_DataSet; } }
+        protected Research.Services.Northwind.NorthwindDataSetServices.IServiceManager NWServiceManager { get { return m_ServiceManager; } }
+        protected Research.Core.Cache.CacheProvider NWCacheProvider { get { return m_CacheProvider; } }
+        protected Dictionary<string, bool> NWIsCached { get { return m_IsCached; } }
+    }
+}
